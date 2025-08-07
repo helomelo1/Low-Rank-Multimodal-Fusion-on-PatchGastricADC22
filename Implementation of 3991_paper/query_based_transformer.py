69e0e68896cc55d403896a12df889bcf644138e0
helomelo1 <b23ee1069@iitj.ac.in>
2025-08-07 11:52:24 +0530

    Implementation of query transformer layer

--- Implementation of 3991_paper/query_based_transformer.py
+++ Implementation of 3991_paper/query_based_transformer.py
@@ -2,3 +2,44 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class TransformerLayer(nn.Module):
+    def __init__(self, d_model, nhead, hidden_dim=2048, dropout=0.1):
+        super().__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=True)
+        self.cross_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=True)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(d_model, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, d_model)
+        )
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+    
+    def forward(self, query, text_embed, image_features):
+        # SELF ATTENTION BLOCK
+        if text_embed is not None: 
+            self_attn_input = torch.cat([query, text_embed], dim = 1)
+        else:
+            self_attn_input = query
+
+        q_norm = self.norm1(query)
+        self_attn_output, _ = self.self_attn(query=q_norm, key=self_attn_input, value=self_attn_input)
+        query = query + self.dropout1(self_attn_output)
+
+        # CROSS ATTENTION BLOCK
+        q_norm = self.norm2(query)
+        cross_attn_output, _ = self.cross_attn(query=q_norm, key=image_features, value=image_features)
+        query = query + self.dropout2(cross_attn_output)
+
+        # FEED FORWARD NETWORK BLOCK
+        q_norm = self.norm3(query)
+        ffn_output = self.feed_forward(q_norm)
+        query = query + self.dropout3(ffn_output)
+
+        return query
